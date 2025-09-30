096f1bdb41b70cc27b5196f9d9c72e6c3867a1f4
Lhtlakshan <thilinalakshanhewage1999@gmail.com>
2025-09-30 09:04:58 +0530

    first commit

---
+++ mypackage/basics.py
@@ -0,0 +1,17 @@
+a = 20
+b = 2.25
+c = "lakshan"
+d = False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print("a + b = ", a + b)   # addition
+print("a - b = ", a - b)   # subtraction
+print("a * b = ", a * b)   # multiplication
+print("a / b = ", a / b)   # division (float result)
+print("a // b = ", a // b)  # floor division (whole number)
+print("a % b = ", a % b)   # remainder
+print("a ** b = ", a ** b)  # power (10^3)
